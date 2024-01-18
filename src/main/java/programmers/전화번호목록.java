32522c828f13f671e2e7b7be800c7b38c1331ad1
SmileJune <godhkekf244@gmail.com>
2024-01-18 20:46:26 +0900

    feat: 프로그래머스 전화번호 목록 문제풀이

--- src/main/java/programmers/전화번호목록.java
+++ src/main/java/programmers/전화번호목록.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
 /**
-*   모든 경우의 수를 하나씩 판단해도 시간이 충분하겠다.
+모든 경우의 수를 하나씩 판단해도 시간이 충분하겠다. O(n^2) -> 최악의 경우 10^12 1000초?
+HashSet을 이용하면 O(1)으로 검색이 가능하다. 하나의 전화번호를 길이 1 ~ (크기-1) 로 잘라 해당 키를 갖고있는 번호가 있는지 확인. 최악의 경우, 번호가 총 10^6개 번호당 20번 확인. 
+더 효율적인 것으로 확인 
 */
 class 전화번호목록 {
     public boolean solution(String[] phone_book) {
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            for(int j = i + 1; j < phone_book.length; j++) {
-                if(isInAWord(phone_book[i], phone_book[j])) {
+        Set<String> set = new HashSet<>();
+        
+        for(String phone_number : phone_book) {
+            set.add(phone_number);
+        }
+        
+        for(String phone_number : phone_book) {
+            for(int i = 0; i < phone_number.length(); i++) {
+                String prefix = phone_number.substring(0, i);
+                if(set.contains(prefix)) {
                     return false;
                 }
             }
@@ -15,15 +24,4 @@ class 전화번호목록 {
         
         return true;
     }
-    
-    private boolean isInAWord(String str1, String str2) {
-        int len = Math.min(str1.length(), str2.length());
-        for(int i = 0; i < len; i++) {
-            if(str1.charAt(i) != str2.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
-
